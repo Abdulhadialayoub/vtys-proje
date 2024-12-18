c17b88eb763588eb13a623303877f5f1d52c83cd
Abdulhadi alayoub <115499325+Abdulhadialayoub@users.noreply.github.com>
2024-12-18 14:08:32 +0300

    unit test is completed

---
+++ restapi/src/test/java/com/rf/coin_analysis/CoinAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rf.coin_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
